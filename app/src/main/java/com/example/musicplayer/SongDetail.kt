45d590669eab946e042a4a972f774c67b959d075
calderonraul <raulcalderon@ravn.co>
2022-04-12 09:44:10 -0600

    Main layout and song detail layout finished

---
+++ app/src/main/java/com/example/musicplayer/SongDetail.kt
@@ -0,0 +1,11 @@
+package com.example.musicplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SongDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_song_detail)
+    }
+}
